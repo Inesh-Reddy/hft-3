87b2b85d34ab978b1d031a6bccb9644f006a242f
Inesh-Reddy <ineshreddy007@gmail.com>
2025-09-21 13:38:16 +0530

    ticker

--- apps/go-services/ticker-service/go.mod
+++ apps/go-services/ticker-service/go.mod
@@ -3,16 +3,11 @@ module github.com/Inesh-Reddy/hft-3/apps/go-services/ticker-service
 go 1.24.4
 
 require (
-	github.com/Inesh-Reddy/hft-3/packages/golib v0.0.0-20250921064630-1516891bdecf
 	google.golang.org/grpc v1.75.1
 	google.golang.org/protobuf v1.36.9
 )
 
 require (
-	github.com/cespare/xxhash/v2 v2.3.0 // indirect
-	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/redis/go-redis/v9 v9.14.0 // indirect
 	golang.org/x/net v0.41.0 // indirect
 	golang.org/x/sys v0.33.0 // indirect
 	golang.org/x/text v0.26.0 // indirect
--- apps/go-services/ticker-service/main.go
+++ apps/go-services/ticker-service/main.go
@@ -1,13 +1,70 @@
 package main
 
 import (
-	pb "github.com/Inesh-Reddy/HFT-3/packages/golib/proto"
+	"encoding/json"
+	"log"
+	"net"
+
+	pb "github.com/Inesh-Reddy/hft-3/packages/proto"
+	"github.com/gorilla/websocket"
+	"google.golang.org/grpc"
 )
 type TickerServer struct {
-	pb.UnimplementedTickerSeerviceServer
-	
+	pb.UnimplementedTickerServiceServer
 }
-func main(){
+
+type BinanceTicker struct {
+	E int64		`json:"E"`
+	S string	`json:"s"`
+	C string	`json:"c"`
+	V string	`json:"v"`
+}
+
+func (s *TickerServer) StreamTicker(req *pb.TickerRequest, stream pb.TickerService_StreamTickerServer) error {
+	url := "wss://stream.binance.com:9443/ws/" + req.Symbol + "@ticker"
+	c, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		return err
+	}
+	defer c.Close()
+
+	for {
+		_, msg, err := c.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return err
+		}
+
+		var t BinanceTicker
+		if err := json.Unmarshal(msg, &t); err != nil {
+			continue
+		}
+
+		update := &pb.TickerUpdate{
+			Exchange:  "binance",
+			Symbol:    t.S,
+			Price:     t.C,
+			Volume:    t.V,
+			EventTime: t.E,
+		}
+
+		if err := stream.Send(update); err != nil {
+			return err
+		}
+	}
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50052")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	grpcServer := grpc.NewServer()
+	pb.RegisterTickerServiceServer(grpcServer, &TickerServer{})
+	log.Println("🚀 Go Ticker Service running on :50052")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
 
 // func main(){
